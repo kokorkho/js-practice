fefaa18a3524da97dfe4bd99424368284653f454
Magomed Shadiev <magomed11175006@mail.ru>
2022-11-09 03:48:01 +0300

    init

---
+++ script.js/main-5.js
@@ -0,0 +1,51 @@
+// var, let, const
+var name = 'Denis';
+console.log(name);
+name = 'Den';
+console.log(mame);
+
+var age;
+console.log(age);
+age = 30;
+console.log(age);
+
+var $mame = 'ivan';
+var _name = 'Maks';
+// var 1name = '';
+
+var userAge = 30;
+var user_age = 30;
+var UserAge = 40;
+var userage = 20;
+
+var car;
+console.log(car);
+car = 'bmw';
+// var car = 'audi';
+console.log(car);
+
+// console.log(nickname);
+let  nickname = 'dmgame';
+nickname = 'dm';
+console.log(nickname);
+
+const firstName = 'Tom';
+firstName = 'Jhon';
+// const lastName;
+
+console.log(firstName);
+
+const user = {
+    mame: 'Denis'
+    age: 30
+};
+
+user.age = 25;
+
+
+console.log(user);
+
+
+
+
+
