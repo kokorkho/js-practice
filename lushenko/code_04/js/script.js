866fe3597b5f5c37ff3dbc9d94d8637362d7c4bd
Magomed Shadiev <magomed11175006@mail.ru>
2023-10-27 20:26:49 +0300

    сделал ДЗ 4

--- lushenko/code_04/js/script.js
+++ lushenko/code_04/js/script.js
@@ -1,13 +1,13 @@
-// document.querySelector('button').onclick = () => {
-//     console.log(document.querySelector('#one').value);
-//     // style
-//     document.querySelector('button').style.backgroundColor = document.querySelector('#one').value
-// }
+document.querySelector('button').onclick = () => {
+    console.log(document.querySelector('#one').value);
+    // style
+    document.querySelector('button').style.backgroundColor = document.querySelector('#one').value
+}
 
-// document.querySelector('#one').oninput = () => {
-//     console.log(document.querySelector('#one').value);
-//     document.querySelector('span').innerHTML = document.querySelector('#one').value
-// }
+document.querySelector('#one').oninput = () => {
+    console.log(document.querySelector('#one').value);
+    document.querySelector('span').innerHTML = document.querySelector('#one').value
+}
 
 document.querySelector('#btn-1').onclick = () => {
     console.log(document.querySelector('#i2').value);
