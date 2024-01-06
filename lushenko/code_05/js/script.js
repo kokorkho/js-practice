8622bb8f7d2a9e694a50090de879b3d730d6fc01
Magomed Shadiev <magomed11175006@mail.ru>
2024-01-06 18:19:59 +0300

    дз5

--- lushenko/code_05/js/script.js
+++ lushenko/code_05/js/script.js
@@ -3,26 +3,26 @@
 
 let div = document.querySelectorAll('.one');
 console.log(div);
-//div.style.background = 'red';
+div.style.background = 'red';
 
-// for (let i = 0; i < div.length; i = i + 1) {
-//     console.log(div[i]);
-//     div[i].style.background = 'red';
-//     div[i].onclick = two;
-// }
+for (let i = 0; i < div.length; i = i + 1) {
+    console.log(div[i]);
+    div[i].style.background = 'red';
+    div[i].onclick = two;
+}
 
-// function two() {
-//     console.log('work!!!');
-// }
+function two() {
+    console.log('work!!!');
+}
 
-// let b = document.getElementsByClassName('one');
-// let c = document.getElementsByTagName('div');
-// console.log(b);
-// console.log(c);
+let b = document.getElementsByClassName('one');
+let c = document.getElementsByTagName('div');
+console.log(b);
+console.log(c);
 
-// for (let i = 0; i < b.length; i++) {
-//     b[i].style.border = '3px solid black';
-// } 
+for (let i = 0; i < b.length; i++) {
+    b[i].style.border = '3px solid black';
+} 
 
 document.querySelector('button').onclick = () => {
     let r = document.querySelectorAll('input[type="radio"]');
