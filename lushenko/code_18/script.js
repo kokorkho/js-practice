38ea8aa86a2711f06b197a5d4ed138f246998518
Magomed Shadiev <magomed11175006@mail.ru>
2024-08-20 23:39:41 +0300

    Дз 18

--- lushenko/code_18/script.js
+++ lushenko/code_18/script.js
@@ -1,15 +1,13 @@
-let a = 'hello,hi,mahai';
-console.log(a.split(','));
-
-let b = [8, 9, 7];
+let a = "hello,hi,mahai";
+console.log(a.split(","));
 
 console.log(b);
-console.log(b.join('-'));
+console.log(b.join("-"));
 
 b.forEach(function (elem, index) {
-    console.log(index);
-    console.log(elem = elem * 2);
-    console.log('--');
+  console.log(index);
+  console.log((elem = elem * 2));
+  console.log("--");
 });
 
 console.log(b);
